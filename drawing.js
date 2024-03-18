14cca400de23832713b47a4098f9d12877646d1d
Venomkajo <kajetanrespondek@gmail.com>
2024-03-19 00:19:27 +0100

    added working grid and drawing

--- drawing.js
+++ drawing.js
@@ -1,19 +1,32 @@
+const rowSize = 16;
 var container = document.getElementById('container');
+var gridPixels;
 
 addPixels(16);
 
-var gridPixels;
+gridPixels.forEach(pixel => {
+    pixel.addEventListener('mouseenter', function(){
+        pixel.style.backgroundColor = 'black';
+})});
 
-function addPixels(amount){
-    for (let i = 0; i < amount; i++){
+function addPixels(pixelAmount){
+    for (let i = 0; i < rowSize; i++){
 
-        var newPixel = document.createElement('div');
-        newPixel.classList.add('pixel');
-        newPixel.textContent = '0';
-        newPixel.style.backgroundColor = 'white';
+        var row = document.createElement('div');
+        row.classList.add('row');
 
-        container.append(newPixel);
+        container.append(row);
+
+        for (let j = 0; j < pixelAmount; j++){
+
+            var newPixel = document.createElement('div');
+            newPixel.classList.add('pixel');
+            newPixel.style.backgroundColor = 'white';
+    
+            row.append(newPixel);
+        }
     }
+    
 
     gridPixels = document.querySelectorAll('.pixel');
 
